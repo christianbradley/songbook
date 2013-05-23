6f8ee87733ecf1e48943ea467a5cad062896cf99
Christian Bradley <christianbradley@gmail.com>
2013-05-23 13:45:37 -0500

    Built new songbooks

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 chordii -T Arial -C Arial-Bold -t 12 -c 9 -G -i *.cho -o songbook.ps
+chordii -T Arial -C Arial-Bold -t 12 -c 9 -G -i covers/*.cho -o covers.ps 
 ps2pdf songbook.ps
+ps2pdf covers.ps 
