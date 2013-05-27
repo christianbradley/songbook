b5de1428fa0d614eea333b82ff0e94cb86ea026b
Christian Bradley <christianbradley@gmail.com>
2013-05-27 08:52:27 -0500

    New Build

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-chordii -T Arial -C Arial-Bold -t 12 -c 9 -G -i *.cho -o songbook.ps
+chordii -T Arial -C Arial-Bold -t 12 -c 9 -G -i originals/*.cho -o originals.ps 
 chordii -T Arial -C Arial-Bold -t 12 -c 9 -G -i covers/*.cho -o covers.ps 
+chordii -T Arial -C Arial-Bold -t 12 -c 9 -G -i ./**/*.cho -o songbook.ps 
 ps2pdf songbook.ps
 ps2pdf covers.ps 
+ps2pdf originals.ps 
